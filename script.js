4c6835c6165ee4174c7d2f264113da0b5c6b0aec
Dorffdaniel <danielcontabill@hotmail.com>
2025-09-10 18:25:31 +0100

    add confirm

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
-/* <!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-  <meta charset="UTF-8">
-  <title>QR Code com JS</title>
-  <script src="https://cdn.jsdelivr.net/npm/qrcodejs/qrcode.min.js"></script>
-</head>
-<body>
-  <div id="qrcode"></div>
 
-  <script>
-    new QRCode(document.getElementById("qrcode"), {
-      text: "https://meusite.com",
-      width: 256,
-      height: 256
-    });
-  </script>
-</body>
-</html>
- */
+
+function cadastrar(){
+  let nome = document.getElementById("nome").value
+
+
+  alert(nome + "cadastrado com sucesso ")
+
+}
